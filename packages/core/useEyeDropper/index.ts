c2d37763d3cab07c923237a7f44621d8786a317c
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-27 13:03:18 +0800

    Revert "fix(useEyeDropper): hydration problem"
    This reverts commit fccac338ba5dab760659d51cac54f8be906c13cd.

--- packages/core/useEyeDropper/index.ts
+++ packages/core/useEyeDropper/index.ts
@@ -1,6 +1,4 @@
 import { ref } from 'vue-demi'
-import { defaultWindow } from '../_configurable'
-import type { ConfigurableWindow } from '../_configurable'
 
 export interface EyeDropperOpenOptions {
   /**
@@ -31,8 +29,8 @@ export interface UseEyeDropperOptions {
  * @see https://vueuse.org/useEyeDropper
  * @param initialValue string
  */
-export function useEyeDropper(options: UseEyeDropperOptions & ConfigurableWindow = {}) {
-  const { initialValue = '', window = defaultWindow } = options
+export function useEyeDropper(options: UseEyeDropperOptions = {}) {
+  const { initialValue = '' } = options
   const isSupported = Boolean(typeof window !== 'undefined' && 'EyeDropper' in window)
   const sRGBHex = ref(initialValue)
 
